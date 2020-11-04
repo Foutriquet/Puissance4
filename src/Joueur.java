e0294401d57e1552f6e091af455f94f3d7ada4ce
Utilisateur <Utilisateur@DESKTOP-KCACBUF.home>
2020-11-04 18:50:00 +0100

    presque fini la partie

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TP3 - Super Puissance
+ * TROUILLET Nicolas - GOMBAULT Mallory 
+ * Jeton - class Joueur
  */
 
 /**
--- src/Partie.java
+++ src/Partie.java
@@ -2,9 +2,9 @@
 import java.util.Random;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TP3 - Super Puissance
+ * TROUILLET Nicolas - GOMBAULT Mallory 
+ * Jeton - class Partie
  */
 
 /**
